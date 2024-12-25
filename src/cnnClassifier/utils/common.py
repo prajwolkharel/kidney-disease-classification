c5593bbd4447aa196812557af4749dff1ab6f000
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-25 12:15:40 +0545

    Created some common functions

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,42 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+
+
+@ensure_annotations
+def read_yaml(path: Path) -> ConfigBox:
+    try:
+        with open(path) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"Yaml file: {path} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(paths:list, verbose=True):
+    for path in paths:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"json file saved at {path}")
+
+
