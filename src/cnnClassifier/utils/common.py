3ca6012826e1635016b8cfc07232d6dca53f0782
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-25 12:22:14 +0545

    Created more utility functions

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -40,3 +40,47 @@ def save_json(path: Path, data: dict):
     logger.info(f"json file saved at {path}")
 
 
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"json file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at path: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) -> ConfigBox:
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
